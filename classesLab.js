a0e9c36cac5f34b72a98b5c33f2a978235821ab3
Jay Fowler <jcafowler@gmail.com>
2019-10-21 21:18:27 -0400

    Complete Problem Two

--- classesLab.js
+++ classesLab.js
@@ -8,6 +8,9 @@ class Person {
     this.lastName = lastName
     this.middleName = middleName
   }
+  fullName() {
+    return `${this.firstName} ${this.middleName} ${this.lastName}`
+  }
 }
 
 let jay = new Person("Javon","Fowler","Charles Anthony")
@@ -16,3 +19,27 @@ let johnny = new Person("Johnny","Appleseed","Fuji")
 console.log (jay["firstName"])
 
 // b. Write a method in `Person` called `fullName` that will return a formatted string of an instance's full name. Call this method on both the instances you created in part a.
+console.log(jay.fullName())
+console.log(johnny.fullName())
+
+// ## Question 2
+
+// a. Create a class called `Book` that has properties `title`, `author` and `rating`. Create some instances of `Book`.
+class Book {
+  constructor(title,author,rating){
+    this.title = title
+    this.author = author
+    this.rating = rating
+  }
+  isGood() {
+    if (this.rating >= 7){
+      return true
+    } else{return false}
+  }
+}
+let novel = new Book("Night", "Eli Wiesel",10)
+let biography = new Book("The Autobiography of Malcom X","Alex Haley and Malcom X",10)
+let kidsBook = new Book("Cat in the Hat","Dr.Seuss",6.5)
+
+// b. Add a method to `Book` called `isGood` that returns `true` if its rating is greater than or equal to 7
+console.log(kidsBook.isGood())
